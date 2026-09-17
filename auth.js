e1979646e0e19d7fab7b7684e4be9ff5b9fae2c9
POS134 <136554775+acosta10032@users.noreply.github.com>
2026-09-17 16:57:50 -0400

    Delete auth.js

--- auth.js
+++
@@ -1,6 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.userId) return next();
-  return res.status(401).json({ error: 'Not signed in.' });
-}
-
-module.exports = { requireAuth };
